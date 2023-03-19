878b6bb30de4a8f6e7f31cffe8ac4e1ff7eac9c1
大聪明 <601670314@qq.com>
2023-03-19 01:43:52 -0700

    Update EventFirer.ts

--- src/EventFirer.ts
+++ src/EventFirer.ts
@@ -1,142 +1,151 @@
-import IEventFirer, {
+import { allFilter, RefWeakMap } from "./global";
+import {
+	IEventFirer, 
 	IListenerItem,
+	TEventFilter,
 	TEventKey,
 	TFilter,
 	TListener,
+	TListenerFilter,
 	TListenersValue
 } from "./interfaces/IEventFirer";
 
 type Constructor<T = Object> = new (...a: any[]) => T;
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const mixin = (Base: Constructor = Object) => {
 	return class EventFirer extends Base implements IEventFirer {
-		#isFire = false;
-		#fireIndex = -1;
-		#offCount: Map<TEventKey, number> = new Map();
 
 		public filters: TFilter[];
-
+	
 		public listeners: Map<TEventKey, TListenersValue>;
-
+	
 		public constructor() {
 			super();
 			this.filters = [];
+			this.listeners = new Map();
 			this.listeners = (this["__proto__" as keyof EventFirer] as any).listeners ?? new Map();
+			RefWeakMap.set(this, {
+				isFire: false,
+				fireIndex: -1,
+				offCount: new Map()
+			});
 		}
-
+	
 		public all(listener: TListener) {
-			return this.filt(() => true, listener);
+			return this.filt(allFilter, listener);
 		}
-
+	
 		public clearListenersByKey(eventKey: TEventKey) {
 			this.listeners.delete(eventKey);
-
+	
 			return this;
 		}
-
+	
 		public clearAllListeners() {
 			const keys = this.listeners.keys();
-
+	
 			for (const key of keys) {
 				this.listeners.delete(key);
 			}
-
+	
 			return this;
 		}
-
-		public filt(rule: Function, listener: TListener) {
+	
+		public filt(rule: TEventFilter, listener: TListenerFilter) {
 			this.filters.push({
 				listener,
 				rule
 			});
-
+	
 			return this;
 		}
-
+	
 		public fire(eventKey: TEventKey | TEventKey[], target?: any) {
 			if (eventKey instanceof Array) {
 				for (let i = 0, len = eventKey.length; i < len; i++) {
 					this.fire(eventKey[i], target);
 				}
-
+	
 				return this;
 			}
-
-			this.#isFire = true;
+	
+			const ref = RefWeakMap.get(this)!;
+	
+			ref.isFire = true;
 			const array: TListenersValue = this.listeners.get(eventKey) || [];
 			// let len = array.length;
 			let item: IListenerItem;
-
+	
 			for (let i = 0; i < array.length; i++) {
-				this.#fireIndex = i;
+				ref.fireIndex = i;
 				item = array[i];
 				item.listener(target);
 				item.times--;
 				if (item.times <= 0) {
 					array.splice(i--, 1);
 				}
-
-				const count = this.#offCount.get(eventKey);
-
+	
+				const count = ref.offCount.get(eventKey);
+	
 				if (count) {
 					// 如果在当前事件触发时，监听器依次触发，已触发的被移除
 					i -= count;
-					this.#offCount.clear();
+					ref.offCount.clear();
 				}
 			}
-
+	
 			this.checkFilt(eventKey, target);
-
-			this.#fireIndex = -1;
-			this.#offCount.clear();
-			this.#isFire = false;
-
+	
+			ref.fireIndex = -1;
+			ref.offCount.clear();
+			ref.isFire = false;
+	
 			return this;
 		}
-
+	
 		public off(eventKey: TEventKey, listener: TListener) {
 			const array: TListenersValue | undefined = this.listeners.get(eventKey);
-
+			const ref = RefWeakMap.get(this)!;
+	
 			if (!array) {
 				return this;
 			}
 			const len = array.length;
-
+	
 			for (let i = 0; i < len; i++) {
 				if (array[i].listener === listener) {
 					array.splice(i, 1);
-					if (this.#isFire && this.#fireIndex >= i) {
-						const v = this.#offCount.get(eventKey) ?? 0;
-
-						this.#offCount.set(eventKey, v + 1);
+					if (ref.isFire && ref.fireIndex >= i) {
+						const v = ref.offCount.get(eventKey) ?? 0;
+	
+						ref.offCount.set(eventKey, v + 1);
 					}
 					break;
 				}
 			}
-
+	
 			return this;
 		}
-
+	
 		public on(eventKey: TEventKey | TEventKey[], listener: TListener) {
 			if (eventKey instanceof Array) {
 				for (let i = 0, j = eventKey.length; i < j; i++) {
 					this.times(eventKey[i], Infinity, listener);
 				}
-
+	
 				return this;
 			}
-
+	
 			return this.times(eventKey, Infinity, listener);
 		}
-
+	
 		public once(eventKey: TEventKey, listener: TListener) {
 			return this.times(eventKey, 1, listener);
 		}
-
+	
 		public times(eventKey: TEventKey, times: number, listener: TListener) {
 			const array: TListenersValue = this.listeners.get(eventKey) || [];
-
+	
 			if (!this.listeners.has(eventKey)) {
 				this.listeners.set(eventKey, array);
 			}
@@ -144,17 +153,17 @@ export const mixin = (Base: Constructor = Object) => {
 				listener,
 				times
 			});
-
+	
 			return this;
 		}
-
+	
 		public checkFilt(eventKey: TEventKey, target: any) {
 			for (const item of this.filters) {
 				if (item.rule(eventKey, target)) {
 					item.listener(target, eventKey);
 				}
 			}
-
+	
 			return this;
 		}
 	};
