27d66828c68efa8790ed624de4c730caad0fe34f
大聪明 <601670314@qq.com>
2023-03-19 01:45:49 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-import { EventFirer } from "./EventFirer";
-import { fire } from "./decorators/fire";
-import { on } from "./decorators/on";
-export * from "./EventFirer";
+export * from "./decorators";
+export * from "./mixin";
 export * from "./interfaces/IEventFirer";
-
-export class A extends EventFirer {
-	@on("aaa")
-	public log(target: any): void {
-		console.log(this, target);
-	}
-
-	@fire("aaa")
-	public dispatch(): void {
-		console.log("???");
-	}
-}
